69bc8d0551bd5356a35433b0d66a0307d64d394e
Михаил <miha061103@gmail.com>
2022-04-19 12:11:09 +0500

    invert feature realization

--- Assets/Scripts/Template.cs
+++ Assets/Scripts/Template.cs
@@ -12,6 +12,7 @@ public enum SchemaType
 public class Template
 {
     public Dictionary<Point, Tribes> Points { get; }
+    public Point RightBottomPoint { get };
     public SchemaType Type { get; }
     public bool IsRotatable { get; }
 
@@ -24,8 +25,42 @@ public class Template
             if (schema[i, j] != Tribes.None)
                 Points[new Point(i, j)] = schema[i, j];
 
+        RightBottomPoint = new Point(, schema.GetLength(1));
         IsRotatable = isRotatable;
         Type = type;
+    }
+
+    public static OffsetToLeftTop()
+    {
+
+    }
+
+    public List<Dictionary<Point, Tribes>> GetSchemas()
+    {
+        var schemas = new List<Dictionary<Point,Tribes>>();
+
+        schemas.Add(Points);
+        if (IsRotatable)
+            return schemas;
+
+        var invertPoints = Dictionary<Point, Tribes>();
+
+        foreach(var i in Points)
+            invertPoints[new Point(i.Key.Y, i.Key.X)] = i.Value;
+        schemas.Add(invertSchema);
+
+        schemas.Add(FlipTemplate(Points));
+        schemas.Add(FlipTemplate(invertPoints));
+    }
+
+    public Dictionary<Point, Tribes> FlipTemplate(Dictionary<Point, Tribes>)
+    {
+        var newPoints = new Dictionary<Point, Tribes>();
+
+        foreach(var i in Points)
+            newPoints[new Point(RightBottomPoint.X - i.Key.X, RightBottomPoint.Y - i.Key.Y)] = i.Value;
+
+        return newPoints;
     }
 }
 
@@ -43,14 +78,17 @@ public class PositionedTemplate
 
     public bool CheckIfMatch(Dictionary<Point, Tile> board)
     {
-        foreach (var i in Template.Points)
-        {
-            var point = new Point(StartingPoint.X + i.Key.X,
-                StartingPoint.Y + i.Key.Y);
-            if (!board.ContainsKey(point) || board[point].occupantTribe != i.Value)
-                return false;
-        }
+        var schemas = Template.GetSchemas();
+        var cnt = schemas.Count;
 
-        return true;
+        foreach (var scheme in sсhemas)
+            foreach (var i in schema) { 
+                var point = new Point(StartingPoint.X + i.Key.X,
+                    StartingPoint.Y + i.Key.Y);
+                if (!board.ContainsKey(point) || board[point].occupantTribe != i.Value)
+                    cnt--;   
+            }
+        
+        return cnt > 0;
     }
 }
