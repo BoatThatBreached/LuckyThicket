d2a5384f687fd0c76e3eec05a1c175ce5d2447ad
Михаил <miha061103@gmail.com>
2022-05-24 12:34:43 +0500

    script for import cards

--- Assets/Cards/import.py
+++ Assets/Cards/import.py
@@ -6,22 +6,35 @@ magpies = pd.read_excel("Cards.xlsx", sheet_name="Magpies")
 
 rarityDict = {"легендарная": 3, "эпическая": 2, "редкая": 1, "обычная": 0}
 
-except_indexes = [13, 19, 21, 22, 34, 36, 37, 38, 44, 45, 46, 49]
+except_indexes = [2, 13, 19, 20, 21, 22, 34, 36, 37, 38, 44, 45, 46, 49]
+
+server_url = "http://a0677209.xsph.ru/test.php"
 
 for index, row in beavers.iterrows():
     ind = row
     if int(ind[3]) not in except_indexes:
-        s = {"Id": int(ind[3]), "Name": str(ind[0]), "Rarity": int(rarityDict[ind[1]]), "AbilityMask": str(ind[2]),
+        # s = "{" + "\"Id\": {0}, \"Name\": {1}, \"Rarity\": {2}, \"AbilityMask\": {3}, \"AbilityString\": {4}" \
+        #      .format(int(ind[3]), str(ind[0]), int(rarityDict[ind[1]]), str(ind[2]), str(ind[4])) + "}"
+        if (str(ind[2]) == 'nan'):
+            ind[2] = ""
+        s = {"Id": int(ind[3]), "Name": ind[0], "Rarity": int(rarityDict[ind[1]]), "AbilityMask": str(ind[2]),
              "AbilityString": str(ind[4])}
 
-        response = re.post("http://a0664388.xsph.ru/test.php", json=s, headers={'Content-type': 'text/plain'
-                                                                                'charset=utf-8'})
+        empty = "{" + "\"Id\": {0}, \"Name\": {1}, \"Rarity\": {2}, \"AbilityMask\": {3}, \"AbilityString\": {4}" \
+            .format(int(ind[3]), "", 0, "", "") + "}"
 
-for index, row in beavers.iterrows():
+        response = re.post(server_url, json=s, headers={"Content-type": "application/json; charset=utf-8"})
+
+for index, row in magpies.iterrows():
     ind = row
     if int(ind[3]) not in except_indexes:
-        s = {"Id": int(ind[3]), "Name": str(ind[0]), "Rarity": int(rarityDict[ind[1]]), "AbilityMask": str(ind[2]),
+        if (str(ind[2]) == 'nan'):
+            ind[2] = ""
+        s = {"Id": int(ind[3]), "Name": str(ind[0]), "Rarity": int(rarityDict[ind[1]]),
+             "AbilityMask": str(ind[2]),
              "AbilityString": str(ind[4])}
 
-        response = re.post("http://a0664388.xsph.ru/test.php", json=s, headers={'Content-type': 'text/plain; '
-                                                                                                'charset=utf-8'})
+        empty = "{" + "\"Id\": {0}, \"Name\": {1}, \"Rarity\": {2}, \"AbilityMask\": {3}, \"AbilityString\": {4}" \
+            .format(int(ind[3]), "", 0, "", "") + "}"
+
+        response = re.post(server_url, json=s)
