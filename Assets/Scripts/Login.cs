bfcb8f7370b91cedfd91bdf4c7f24a2701adc62f
Михаил <miha061103@gmail.com>
2022-05-27 23:36:02 +0500

    stay in system logic

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -1,15 +1,20 @@
+using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Login : MonoBehaviour
 {
+    private const string CredentialsPath = "credentials";
     public TMP_Text duckPassword;
     public Popup popup;
     public TMP_InputField login;
     public TMP_InputField password;
+    public Toggle check;
     public GameObject tribeChoice;
     private bool _hidden = true;
 
@@ -19,6 +24,13 @@ public class Login : MonoBehaviour
 
     private void Start()
     {
+        var tuple = LoadCredentials();
+        if (tuple.Item1 != "")
+        {
+            Account.Load(tuple.Item1, tuple.Item2);
+            SceneManager.LoadScene("MenuScene");
+        }
+
         AudioStatic.MenuInitSounds(this, gameObject);
     }
 
@@ -44,6 +56,8 @@ public class Login : MonoBehaviour
         if (!IsInputCorrect() || !TryConnect(out var token))
             return;
         ShowSuccess("Logged in successfully.\nRedirecting.");
+        if (check.isOn)
+            SaveCredentials(login.text, token);
         Account.Load(login.text, token);
         SceneManager.LoadScene("MenuScene");
     }
@@ -127,5 +141,38 @@ public class Login : MonoBehaviour
     private void ShowError(string message)
     {
         popup.ShowMessage(message, Color.red);
+    }
+
+    public static void SaveCredentials(string login, string token)
+    {
+        using FileStream fs = File.Create(CredentialsPath);
+        var info = new UTF8Encoding(true).GetBytes(login + " " + token);
+        fs.Write(info, 0, info.Length);
+    }
+    
+    public static void ClearCredentials()
+    {
+        using FileStream fs = File.Create(CredentialsPath);
+        var info = new UTF8Encoding(true).GetBytes("");
+        fs.Write(info, 0, info.Length);
+    }
+
+    private static Tuple<string, string> LoadCredentials()
+    {
+        try
+        {
+            using StreamReader sr = File.OpenText(CredentialsPath);
+            var s = sr.ReadLine();
+            if (string.IsNullOrEmpty(s))
+                return Tuple.Create("", "");
+
+            var p = s.Split(' ');
+
+            return Tuple.Create(p[0], p[1]);
+        }
+        catch
+        {
+            return Tuple.Create("", "");
+        }
     }
 }
